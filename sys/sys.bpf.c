d9462e8fb1ae3c18d0886353e487a0b2a85ce93a
JDLu2003 <1277319667@qq.com>
2025-07-14 10:28:02 +0800

    bug: auto update

--- sys/sys.bpf.c
+++ sys/sys.bpf.c
@@ -6,19 +6,40 @@
 #include "user_impl.inc"
 #include "user_interface.h"
 
+typedef __u32 u32;
+typedef char stringkey[64];
+
+struct {
+  __uint(type, BPF_MAP_TYPE_HASH);
+  __uint(max_entries, 128);
+  strintkey *key;
+  __type(value, u32);
+} sys_map SEC(".maps");
+
+static __always_inline int check_pid() {
+  stringkey key = "pid";
+  u32 mypid = bpf_get_current_pid_tgid();
+  u32 *val = bpf_map_lookup_elem(&sys_map, &key);
+  if (val && *val == mypid)
+    return 1;
+  else
+    return 0;
+}
+
 char LICENSE[] SEC("license") = "GPL";
 
 SEC("kprobe/do_sys_openat2")
 int handle_open(struct pt_regs *ctx) {
-  int dfd = PT_REGS_PARM1(ctx);
-  const char *filename = (const char *)PT_REGS_PARM2(ctx);
-  struct open_how *how = (struct open_how *)PT_REGS_PARM3(ctx);
+  if (check_pid()) {
+    int dfd = PT_REGS_PARM1(ctx);
+    const char *filename = (const char *)PT_REGS_PARM2(ctx);
+    struct open_how *how = (struct open_how *)PT_REGS_PARM3(ctx);
 
-  char fname[256];
-  bpf_probe_read_user_str(fname, sizeof(fname), filename);
+    char fname[256];
+    bpf_probe_read_user_str(fname, sizeof(fname), filename);
 
-  if (callbacks.on_open)
-    callbacks.on_open(fname);
-
+    if (callbacks.on_open)
+      callbacks.on_open(fname);
+  }
   return 0;
 }
---
+++ sys/sys.c
@@ -0,0 +1,86 @@
+#include "sys.skel.h"
+#include <bpf/libbpf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+typedef char stringkey[64];
+typedef __u32 u32;
+
+int main(int argc, char **argv) {
+
+  struct timespec start_time, end_time;
+
+  // clear all cache
+  int ret = system("sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'");
+
+  if (ret == -1) {
+    fprintf(stderr, "Failed to clear all cache\n");
+  } else {
+    fprintf(stderr, "Succeessfil cleared all caches\n");
+  }
+
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <command> [args ...]\n", argv[0]);
+    return 1;
+  }
+
+  struct sys_bpf *skel;
+  int err;
+  skel = sys_bpf__open();
+  if (!skel) {
+    fprintf(stderr, "Failed to open BPF skeleton\n");
+    return 1;
+  }
+  err = sys_bpf__load(skel);
+  if (err) {
+    fprintf(stderr, "Failed to load BPF skeleton: %d\n", err);
+    goto cleanup;
+  }
+  err = sys_bpf__attach(skel);
+  if (err) {
+    fprintf(stderr, "Failed to attach BPF skeleton: %d\n", err);
+    goto cleanup;
+  }
+
+  pid_t child = fork();
+  if (child < 0) {
+    perror("fork");
+    goto cleanup;
+  }
+
+  if (child == 0) {
+    // 子进程：执行测试命令
+    execvp(argv[1], &argv[1]);
+    perror("execvp");
+    exit(127);
+  }
+
+  clock_gettime(CLOCK_MONOTONIC, &start_time);
+
+  // 父进程：将子进程 pid 写入 bpf map
+  stringkey keys = "pid";
+  u32 zero = 0;
+  bpf_map__update_elem(skel->maps.bench_map, &keys[0], sizeof(keys[0]), &child,
+                       sizeof(child), BPF_ANY);
+
+  printf("Running '%s' (pid=%d), monitoring...\n", argv[1], child);
+  printf("check bpf output in '/sys/kernel/debug/tracing/trace_pipe'\n");
+  int status;
+  while (1) {
+    // 检查子进程是否结束
+    pid_t w = waitpid(child, &status, 0);
+    if (w == child) {
+      printf("Child process exited.\n");
+      break;
+    }
+    sleep(1);
+    printf(".");
+  }
+
+cleanup:
+  sys_bpf__destroy(skel);
+  return 0;
+}
