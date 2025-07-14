e591c7f1e9648e4039556f0f1b881f3f96080834
JDLu2003 <1277319667@qq.com>
2025-07-14 09:56:43 +0800

    bug: auto update

--- sys/user_interface.h
+++ sys/user_interface.h
@@ -5,7 +5,8 @@ struct user_callbacks {
   void (*on_open)(const char *filename);
 }
 
-__attrbute__((weak)) void default_on_open(const char *filename) {
+__attribute__((weak)) void
+default_on_open(const char *filename) {
   bpf_printk("default on_open: %s\n", filename);
 }
 
